f4d916613ad3baf96d23e2737e1ed3dde19e63e0
Adam Wernström <adam.wernstrom@gmail.com>
2017-09-19 17:06:38 +0200

    "First" upload...

---
+++ Arbetsblad2/Uppgift5/main.c
@@ -0,0 +1,30 @@
+/*
+Uppgift 5.
+This code takes two strings and combines them into one and then printing them out.
+*/
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char* create_headline(const char *s1, const char *s2) {
+	char *result = malloc(sizeof(char) * (strlen(s1) + strlen(s2) + 1));
+	strcpy(result, s1);
+	strcat(result, s2);
+
+	return result;
+}
+
+
+int main()
+{
+	char *headline = "";
+	char *title = "Nackademin C/C++ ";
+	char *subtitle = "Year 1";
+	headline = create_headline(title, subtitle);
+
+	printf("%s\n", headline);
+
+	return 0;
+}
---
+++ Arbetsblad2/Uppgift6/main.c
@@ -0,0 +1,16 @@
+/*
+Uppgift 6.
+
+*/
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+
+int main()
+{
+	return 0;
+}
