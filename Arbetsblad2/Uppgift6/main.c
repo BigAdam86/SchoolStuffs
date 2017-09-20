7c743b7c449543f58f0e2a4634450beb411c5a53
BigAdam86 <31543595+BigAdam86@users.noreply.github.com>
2017-09-20 16:56:47 +0200

    Uppgift6 Arbetsblad2 updated NOT FINAL

--- Arbetsblad2/Uppgift6/main.c
+++ Arbetsblad2/Uppgift6/main.c
@@ -8,9 +8,45 @@ Uppgift 6.
 #include <stdlib.h>
 #include <string.h>
 
+float convert_to_fahrenheit(const float * dC) {					//, const float *dF
+	
+	dF = ((9/5) * *dC) + 32;
 
+	return dF;
+}
+
+
+int main() 
+{
+	float * Celsius = 50;
+	float * Fahrenheit = convert_to_fahrenheit(*Celsius);
+	printf("%f\n", *Fahrenheit);
+	return 0;
+}
+
+
+
+
+
+/*
+//			Haitams
+//---------------------------
+typedef float flyt;
+
+
+flyt FahrToCell(flyt temp) {
+
+
+	flyt celsius = (5.0 / 9.0) * (temp - 32);
+
+	return celsius;
+}
 
 int main()
 {
+	flyt temprature = 20;
+	temprature = FahrToCell(temprature);
+	printf("%.1f", temprature);
 	return 0;
 }
+*/
